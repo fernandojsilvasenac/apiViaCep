f21dd72ddbe1fc2e88b1491bedebce19bd664f3e
fernandojsilvasenac <fernando.jsilvasenac@gmail.com>
2020-04-06 08:51:19 -0300

    my first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CepService } from '../service/cep.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  cep: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  
+
+  constructor(private cepService: CepService ) { }
+
+  async consultaCEP() {
+    try {
+      const result = await this.cepService.getEndereco(this.cep);
+      this.populaDadosCep(result)
+    } catch (error) {
+    console.log(error);
+    }
+  }
+
+  // Sem async/await
+  // consultaCep(){
+  //   if (this.cep != ''){
+  //     const result = this.cepService.getEndereco(this.cep);
+  //         this.populaDadosCep(dados))
+  //     }
+  // }
+
+  populaDadosCep(dados) {
+      this.logradouro = dados.logradouro;
+      this.complemento = dados.complemento;
+      this.bairro = dados.bairro;
+      this.cidade = dados.localidade;
+      this.estado = dados.uf;
+  }
 
 }
